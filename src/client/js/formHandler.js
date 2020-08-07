a1aa75a19aacceb1dde05fa56bb47527bc7cb96f
RLant <rebeccalant@gmail.com>
2020-08-06 21:06:08 -0400

    Adding our names with our snacks

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,15 +5,21 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
 
     Client.checkForName(formText)
-
+//THis is the javascript fetch request!!//
+//when the fetch request runs, it follows these steps//
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
+
     .then(res => {
+      console.log(res);
+      console.log(res.json());
         return res.json()
-    })
+    })//then is takes the data from the funtction and inputs it to run this function.
     .then(function(data) {
+                                        //.message can be .title or .time, just to change and rebuild.
         document.getElementById('results').innerHTML = data.message
     })
 }
 
 export { handleSubmit }
+}
--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,15 +1,19 @@
 function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ]
+    let justin = "Justin";
+    let becca = "Becca";
+    let nova = "Nova";
 
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!")
+    if(justin.includes (inputText)) {
+        alert("Welcome, Justin! Have an Oreo!")
+    }
+
+    if(becca.includes (inputText)) {
+        alert("Welcome, Becca! Have a dragon sushi roll!")
+    }
+
+    if(nova.includes (inputText)) {
+        alert("Welcome, Nova! Have some chicken!")
     }
 }
 
